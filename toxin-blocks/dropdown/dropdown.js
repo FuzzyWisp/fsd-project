f1d95ef7a448b19e51a0c12806b55882926ff0aa
FuzzyWisp <kirill.zagrebnev@yandex.ru>
2020-05-14 23:15:01 +0300

    start to making header

--- toxin-blocks/dropdown/dropdown.js
+++ toxin-blocks/dropdown/dropdown.js
@@ -1,14 +1,14 @@
 let dropdown = function(){
-    let dropdownArr = document.querySelectorAll('.dropdown');
-    
-    dropdownArr.forEach(item => {
-      item.addEventListener('click', function(){
-        let dropdownBody = this.querySelector('.dropdown__body');
-        let dropdownMenu = this.querySelector('.dropdown__menu');
-        dropdownMenu.classList.toggle('dropdown__menu_active');
-        dropdownBody.classList.toggle('dropdown__body_active');
-      })
-    })
-  };
+  let dropdownArr = document.querySelectorAll('.dropdown');
   
-dropdown();
+  dropdownArr.forEach(item => {
+    item.addEventListener('click', function(){
+      let dropdownBody = this.querySelector('.dropdown__body');
+      let dropdownMenu = this.querySelector('.dropdown__menu');
+      dropdownMenu.classList.toggle('dropdown__menu_active');
+      dropdownBody.classList.toggle('dropdown__body_active');
+    })
+  })
+};
+  
+dropdown(); 
